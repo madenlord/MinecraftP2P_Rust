01bc2ba7c93517fba5727ae6bed9469925929381
madenlord <madenlordhobbies@gmail.com>
2023-10-10 22:11:21 +0200

    [DEV] Implemented Git command functions

--- src/server/repo.rs
+++ src/server/repo.rs
@@ -1,47 +1,55 @@
+mod repo {
+    const SAVE_PATH: &str = "conf/repo.conf";
 
-// TODO: Implement the following functions:
-//      + host: to host, the user will need to verify
-//              the server is not already hosted. That
-//              means, pulling the latest changes and
-//              checking the content of the .lock file
+    mod git {
+        use std::process::ExitStatus;    
 
-pub mod git {
-    use std::process::ExitStatus;    
+        use crate::server::ioutils::terminal;
 
-    use crate::server::ioutils::terminal;
+        // =========== GET AND UPDATE STATE ===========
+        pub fn status() -> Result<ExitStatus, std::io::Error>{
+            execute_git_command("status", vec![])
+        }
 
-    // =========== GET AND UPDATE STATE ===========
-    pub fn status() -> Result<ExitStatus, std::io::Error>{
-        execute_git_command("status", vec![])
-    }
+        pub fn fetch() -> Result<ExitStatus, std::io::Error>{
+            execute_git_command("fetch", vec![])
+        }
 
-    pub fn fetch() {
-
-    }
-
-    pub fn pull() {
-
-    }
-
-    
-
-    // =========== PUSH ===========
-    pub fn add() {
-
-    }
+        pub fn pull() -> Result<ExitStatus, std::io::Error>{
+            execute_git_command("pull", vec![])
+        }
 
 
-    pub fn commit() {
 
-    }
+        // =========== PUSH ===========
+        pub fn add(files: Vec<&str>) -> Result<ExitStatus, std::io::Error>{
+            execute_git_command("add", files)
+        }
 
-    pub fn push() {
 
-    }
+        pub fn commit(message: &str) -> Result<ExitStatus, std::io::Error>{
+            execute_git_command("commit", vec!["-m", message])
+        }
 
-    fn execute_git_command<'a>(command: &'a str, mut args: Vec<&'a str>) -> Result<ExitStatus, std::io::Error>{ 
-        let mut git_args: Vec<&str> = vec![command];
-        git_args.append(&mut args);
-        terminal::execute_command("git", git_args, ".")
+        pub fn push() -> Result<ExitStatus, std::io::Error>{
+            execute_git_command("push", vec![])
+        }
+
+
+
+        // =========== SETTERS ===========
+        pub fn set_origin(url: &str) -> Result<ExitStatus, std::io::Error> {
+            execute_git_command("remote", vec!["set-url", "origin", url])
+        }
+
+
+
+        fn execute_git_command<'a>(command: &'a str, mut args: Vec<&'a str>) -> Result<ExitStatus, std::io::Error>{ 
+            let mut git_args: Vec<&str> = vec![command];
+            git_args.append(&mut args);
+            terminal::execute_command("git", git_args, ".")
+        }
     }
 }
+
+
